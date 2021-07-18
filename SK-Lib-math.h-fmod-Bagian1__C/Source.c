41b87bb13ebe4601d772070f062fb6f3fe4e9dc6
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-18 10:19:55 +0700

    Add project files.

---
+++ SK-Lib-math.h-fmod-Bagian1__C/Source.c
@@ -0,0 +1,10 @@
+#include <stdio.h>      /* printf */
+#include <math.h>       /* fmod */
+#include <conio.h>
+
+int main() {
+	printf("fmod of 8.6 / 4 is %f\n", fmod(8.6, 4));
+	printf("fmod of 89 / 7.3 is %f\n", fmod(89, 7.3));
+	_getch();
+	return 0;
+}
